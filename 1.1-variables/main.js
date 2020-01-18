76aeb17c600e392478befd65bd6e74c32090f0c9
Dm DSK <dsk4post@gmail.com>
2020-01-18 17:22:46 +0400

    first calculate average

---
+++ 1.1-variables/main.js
@@ -0,0 +1,7 @@
+// Подсчёт среднего значения
+let algebra = 3;
+let geography = 5;
+let physics = 3;
+let TotalSubjects = 3;
+let AverageMark = (algebra + geography + physics) / TotalSubjects;
+console.log("Среднее значение : " + AverageMark.toFixed(2));
