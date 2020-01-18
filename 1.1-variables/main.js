17a6df0d2b0884fc30fac96b842d8d0094b098f8
Dm DSK <dsk4post@gmail.com>
2020-01-18 19:25:19 +0400

    second message myname

--- 1.1-variables/main.js
+++ 1.1-variables/main.js
@@ -1,7 +1,12 @@
-// Подсчёт среднего значения
+// Пример 1. Подсчёт среднего значения
 let algebra = 3;
 let geography = 5;
 let physics = 3;
 let TotalSubjects = 3;
 let AverageMark = (algebra + geography + physics) / TotalSubjects;
 console.log("Среднее значение : " + AverageMark.toFixed(2));
+
+// Пример 2. Вывод имени по шаблону
+let myname = "Дмитрий";
+let message = "Привет, мир! Меня зовут " + myname;
+console.log(`${message}`);
