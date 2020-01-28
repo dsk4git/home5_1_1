c2f3943513ae622ee7cfe9eb008c836c72cceda8
Dm DSK <dsk4post@gmail.com>
2020-01-29 02:10:33 +0400

    Тема 3.Задание 1.Часть 2.Корни квадратного уравнения.

--- 1.3-functions/main.js
+++ 1.3-functions/main.js
@@ -1,28 +1,70 @@
 "use strict";
-let rez = "";
+let rez;
 
 function getSolutions(a,b,c){
     let D = (Math.pow(b,2) - 4*a*c);
-    let DiskrSqrt = 0,
+    let diskrSqrt = 0,
         x1 = 0,
         x2 = 0,
-        x="";
+        x = new Object();
 
     if (D > 0) {
-        DiskrSqrt = Math.sqrt(D);
-        x1 = ((-b + DiskrSqrt) / (2 * a)).toFixed(2);
-        x2 = ((-b - DiskrSqrt) / (2 * a)).toFixed(2);
-        x = ('{ ' + 'roots: ' + '[x1: ' + x1 + ', x2: ' + x2 + '], ' + 'D: ' + D + ' }');
+        diskrSqrt = Math.sqrt(D);
+        x1 = ((-b + diskrSqrt) / (2 * a)).toFixed(2);
+        x2 = ((-b - diskrSqrt) / (2 * a)).toFixed(2);
+        x.roots = [x1,x2];
+        x.roots[0] = [x1];
+        x.roots[1] = [x2];
+        x.D = D;
     }
     else if (D === 0) {
-        x1 = [((-b) / (2 * a)).toFixed(2)];
-        x = ('{ ' + 'roots: ' + '[' + x1 + '], ' + 'D: ' + D + ' }');
+        x1 = ((-b) / (2 * a)).toFixed(2);
+        x.roots = [x1];
+        x.roots[0] = x1;
+        x.D = D;
     }
     else {
-         x = ('{ ' + 'D: ' + D + ' }');
-    };
+         x.D = D;
+    }
     return(x);
 }
 
-rez = getSolutions(2,3,-2);
-console.log(rez);
+function showSolutionsMessage(a,b,c){
+    
+    let result = new Object();
+
+    result = getSolutions(a,b,c);
+    if (c < 0){
+        if (b < 0){
+            console.log("Вычисляем корни квадратного уравнения "+ a + 'x**2 ' + b + 'x' + c);
+        }
+        else{
+            console.log("Вычисляем корни квадратного уравнения "+ a + 'x**2 +' + b + 'x' + c);
+        }
+    }
+    else{
+        if (b < 0){
+            console.log("Вычисляем корни квадратного уравнения "+ a + 'x**2 ' + b + 'x +' + c);
+        }
+        else{
+            console.log("Вычисляем корни квадратного уравнения "+ a + 'x**2 +' + b + 'x +' + c);
+        }
+    }
+    console.log("Значение дискриминанта: " + result.D);
+    switch (true) {
+        case (result.D < 0):
+            console.log("Уравнение не имеет вещественных корней");
+            break;
+        case (result.D === 0):
+            console.log("Уравнение имеет один корень X1 = " + result.roots[0]);
+            break;
+        default:
+            console.log("Уравнение имеет два корня. X1 = " + result.roots[0] + ", X2 = " + result.roots[1]);
+            break;
+    }
+//    return(x);
+}
+
+rez = showSolutionsMessage(2,-3,-2);
+rez = showSolutionsMessage(2,4,2);
+rez = showSolutionsMessage(2,3,2);
